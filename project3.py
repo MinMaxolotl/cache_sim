332b98a0885b468d6c240d587e59a5b84f947863
maxwell kawada <kmaxwell21@vt.edu>
2024-04-22 18:53:06 -0400

    Added header, outlines for parsing each line, and added example inputs and outputs

--- project3.py
+++ project3.py
@@ -1,3 +1,20 @@
+#############################################
+# Name: project3.py
+# Author: Maxwell Kawada
+# Last Edited: 4/22/2024
+# Purpose: To simulate a cahce and the hit rate for different
+#          block sizes using a command line tool
+#############################################
+
+# The basic cache is
+# • Cache Size: 2048 Bytes
+# • Block Size: 2 words, or 8 bytes
+# • Cache Placement Type: direct mapped
+# • Write Policy: write back
+
+# I will test Block Sizes: 2, 4, 8, 32 words
+# Blocks in bytes        : 8, 16, 32, 128 bytes
+
 # Argparser tutorial from https://www.codium.ai/blog/creating-powerful-command-line-tools-in-python-a-practical-guide/
 import argparse
 parser = argparse.ArgumentParser(description='This is a new command-line tool') # Creates an argparser object
@@ -10,3 +27,8 @@ args = parser.parse_args()                                                      
 # https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/#
 input = open(args.input_file, 'r')
 Lines = input.readlines()
+
+
+
+for line in Lines:
+    
