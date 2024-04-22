606e04e96f1d90458e8935e85f613a7bffbcef2f
maxwell kawada <kmaxwell21@vt.edu>
2024-04-22 18:00:56 -0400

    Imported argparse, added arguement for input file

--- project3.py
+++ project3.py
@@ -1,1 +1,8 @@
+# Argparser tutorial from https://www.codium.ai/blog/creating-powerful-command-line-tools-in-python-a-practical-guide/
 import argparse
+parser = argparse.ArgumentParser(description='This is a new command-line tool') # Creates an argparser object
+parser.add_argument('input_file', help='Path to the input file')                # Adds an arguement to accept an input file
+args = parser.parse_args()                                                      # Gathers arguements that can be called from args object
+
+# Now we create a method of parsing through the input file
+# We only need to recognize the keyword "read" and "write"
