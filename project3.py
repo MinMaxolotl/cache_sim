d6c4880de7ee30a6d0373beb67667f2c12579ceb
maxwell kawada <kmaxwell21@vt.edu>
2024-04-22 23:26:35 -0400

    Added calculations for number of tag, set index, and offset bits to read each address

--- project3.py
+++ project3.py
@@ -21,6 +21,37 @@ parser = argparse.ArgumentParser(description='This is a new command-line tool') 
 parser.add_argument('input_file', help='Path to the input file')                # Adds an arguement to accept an input file
 args = parser.parse_args()                                                      # Gathers arguements that can be called from args object
 
+# Next I will initalize the states for all of the caches I will be testing
+c_size = 1024
+b_size = [8, 16, 32, 128]
+
+# Address bits: 8 bytes = 32 bits
+# Size of each way = cache_ size / # ways 
+# • Only using direct mapping (# ways = 1), so size of each way always = cache_size = 1024
+# Number of blocks = size of each way / block size (bytes)
+
+num_blocks = [128, 64, 32, 8]
+
+# Bits for offset comes from block size = 2^x, where x = # offset bits
+
+offset_bits = [3, 4, 5, 7]
+
+# Bits for index: number of blocks = 2^y, where y = # of index bits
+
+index_bits = [7, 6, 5, 3]
+
+# Tag bits = 32 bits - offset_bits - tag_bits = 32 - 10 = 22 bits
+
+tag_bits = 22
+
+# Order of address goes as such: tag -> index -> offset
+
+
+# I now create a function that will perform the bulk of the work
+
+def parse_and_eval(line):
+
+
 # Now we create a method of parsing through the input file
 # We only need to recognize the keyword "read" and "write"
 
@@ -31,4 +62,6 @@ Lines = input.readlines()
 
 
 for line in Lines:
+    print("test")
+
     
