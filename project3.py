792e645067f38671083c04d923bc2dbf1188d584
maxwell kawada <kmaxwell21@vt.edu>
2024-04-24 18:15:46 -0400

    giving up for today. read and write kind of work? but index always seems to be zero and thats odd. thus the tag keeps changing so read never makes a hit, my hit ratiois wrong then

--- project3.py
+++ project3.py
@@ -25,7 +25,8 @@ args = parser.parse_args()                                                      
 # Next I will initalize the states for all of the caches I will be testing
 c_size = 1024
 b_size = [8, 16, 32, 128]
-blocks_per_index = [2, 4, 8, 32]
+words_per_index = [2, 4, 8, 32]
+hit_attempts = 32
 
 # Address bits: 8 bytes = 32 bits
 # Size of each way = cache_ size / # ways 
@@ -49,11 +50,11 @@ tag_bits = 22
 # Order of address goes as such: tag -> index -> offset
 
 # Next I want to set up all the other variables that I will be printing after a read or write completes
-mapping_type = "DM" # Always DM in my case
-write_policy = "WB" # Alwaays WB in my case
-hit_rate = 0.00     # This will be modified, percentage of cache hit
-MemToCache = 0      # This will be modified, number of bytes put into cache from memory
-CacheToMem = 0      # This will be modified, number of bytes put into memory from cache
+mapping_type = "DM"     # Always DM in my case
+write_policy = "WB"     # Alwaays WB in my case
+hit_rate = [0, 0, 0, 0] # This will be modified, percentage of cache hit
+MemToCache = 0          # This will be modified, number of bytes put into cache from memory
+CacheToMem = 0          # This will be modified, number of bytes put into memory from cache
 
 # Other output variables is cache size, block size, and number of blocks, which are all created above
 
@@ -66,54 +67,83 @@ CacheToMem = 0      # This will be modified, number of bytes put into memory fro
 # I also include a verify column to keep track of used indexes
 
 
-cache_data = [[{'tag': None, 'data': [None] * blocks_per_index[0]} for x in range(num_blocks[0])], 
-              [{'tag': None, 'data': [None] * blocks_per_index[1]} for x in range(num_blocks[1])], 
-              [{'tag': None, 'data': [None] * blocks_per_index[2]} for x in range(num_blocks[2])], 
-              [{'tag': None, 'data': [None] * blocks_per_index[3]} for x in range(num_blocks[3])]]
+cache_data = [[{'tag': None, 'data': [None] * words_per_index[0]} for x in range(num_blocks[0])], 
+              [{'tag': None, 'data': [None] * words_per_index[1]} for x in range(num_blocks[1])], 
+              [{'tag': None, 'data': [None] * words_per_index[2]} for x in range(num_blocks[2])], 
+              [{'tag': None, 'data': [None] * words_per_index[3]} for x in range(num_blocks[3])]]
 
 v_col = [[None] * num_blocks[0], [None] * num_blocks[1], [None] * num_blocks[2], [None] * num_blocks[3]]
 
 # I now create a function that will perform the bulk of the work
 def read(address, setting):
-    print("reading")
+    #print("reading") # Debug Print Statement
     global MemToCache
+    global hit_rate
 
     # Caclcualate the tag, block index, and block offset so we can check if the cache contains the data
-    tag = int(address, 16) // c_size
+    tag = int(address, 16) // c_size 
     block_index = (int(address, 16) // b_size[setting]) % num_blocks[setting]
-    block_offset = int(address, 16) % b_size[setting]
+    block_offset = int(address, 16) % words_per_index[setting]
 
-    print(address)
-    print(tag)
-    print(block_index)
-    print(block_offset)
+    # print(address)        # Debug Print Statements
+    # print(format(int(address, 16), 'b'))
+    # print(f"looking for tag: {tag}")
+    # print(f"looking for index: {block_index}")
+    # print(f"looking for offset: {block_offset}")
+
+    # print(f"found tag: {cache_data[setting][block_index]['tag']}")
+    # print(f"verify bit: {v_col[setting][block_index]}")
 
     #If theres nothing in the cache at that location, then its a cache miss
     #If the verify bit is zero at the block index, then its a cache miss
     #If the tags do not match at the block index, then its a cache miss
         
-    if ((v_col[setting][block_index] == 0) or (tag != cache_data[setting][block_index]['tag']) or (cache_data[setting][block_index]['data'][block_offset] == None)): 
+    if ((v_col[setting][block_index] == 0) or (tag != cache_data[setting][block_index]['tag'])): 
         # When there is a cache miss, we pull data from memory and fill the cache, I use a filler value of "fake_data"
-        MemToCache += + b_size[setting]
+        # print("miss!")
+        MemToCache += b_size[setting]
+        cache_data[setting][block_index]['data'] = ["fake_data"] * (words_per_index[setting]) # For each block at this index, we fill in the data 
 
-        cache_data[setting][block_index]['data'] = ["fake_data"] * (blocks_per_index[setting]) # For each block at this index, we fill in the data 
+        #print(cache_data[setting][block_index]['data'])
+
         cache_data[setting][block_index]['tag'] = tag # We set the tag value as the one we solved for earlier
         v_col[setting][block_index] = 1 # after filling the blocks, we set v = 1
+
+    else:
+        hit_rate[setting] += 1
+        print("hit!")
         
         
 
-def write(address, setting): # We use the write back methodology
-    print("writing")
+def write(address, setting): # We use the write back methodology#
+    #print("writing")
+    global MemToCache
+    global CacheToMem
+
+    tag = int(address, 16) // c_size 
+    block_index = (int(address, 16) // b_size[setting]) % num_blocks[setting]
+    block_offset = int(address, 16) % words_per_index[setting]
+
+    if cache_data[setting][block_index]['tag'] == tag:
+        cache_data[setting][block_index]['data'][block_offset] = "fake_data"
+        v_col[setting][block_index] = 1
+        hit_rate[setting] += 1
+    else:
+        MemToCache += 1
+
+    CacheToMem += 1
 
 def results(setting):
     # Print the final results
-    global hit_rate
+    global hit_rate 
     global MemToCache
     global CacheToMem
-    print(f"{c_size} {b_size[setting]} {mapping_type} {write_policy} {hit_rate} {MemToCache} {CacheToMem}")
+
+    hit_rate[setting] = hit_rate[setting]/hit_attempts
+
+    print(f"{c_size} {b_size[setting]} {mapping_type} {write_policy} {hit_rate[setting]} {MemToCache} {CacheToMem}")
 
     # reset the values back to initial state
-    hit_rate = 0.00     # This will be modified, percentage of cache hit
     MemToCache = 0      # This will be modified, number of bytes put into cache from memory
     CacheToMem = 0      # This will be modified, number of bytes put into memory from cache
     
